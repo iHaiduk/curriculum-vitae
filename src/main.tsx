24524fd8c1433b8b29c918c5f2b7a29efabcc2be
Ihor Haiduk <cookkycok@gmail.com>
2025-02-09 22:22:13 +0200

    Updated build

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import 'semantic-ui-css/semantic.min.css'
 
 import './main.css'
 
 import App from './App.tsx'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
